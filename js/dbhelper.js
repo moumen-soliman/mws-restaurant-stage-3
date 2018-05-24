7f27a387ba88cd15f0f15cc313bd0e29fb86123d
Mo'men Soliman <moumensoliman@gmail.com>
2018-05-24 22:36:00 +0200

    add DB helpers

---
+++ js/dbhelper.js
@@ -0,0 +1,196 @@
+/**
+ * Common database helper functions.
+ */
+class DBHelper {
+  /**
+   * Database URL.
+   * Change this to restaurants.json file location on your server.
+   */
+  static get DATABASE_URL() {
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
+  }
+  /**
+   * Fetch all restaurants.
+   */
+  static fetchRestaurants(callback) {
+    IDBHelper.readAllIdbData(IDBHelper.dbPromise)
+      .then(restaurants => { return callback(null, restaurants) });
+  }
+  /**
+   * Fetch a restaurant by its ID.
+   */
+  static fetchRestaurantById(id, callback) {
+    // fetch all restaurants with proper error handling.
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const restaurant = restaurants.find(r => r.id == id);
+        if (restaurant) { // Got the restaurant
+          callback(null, restaurant);
+        } else { // Restaurant does not exist in the database
+          callback('Restaurant does not exist', null);
+        }
+      }
+    });
+  }
+
+  /**
+   * Fetch restaurants by a cuisine type with proper error handling.
+   */
+  static fetchRestaurantByCuisine(cuisine, callback) {
+    // Fetch all restaurants  with proper error handling
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Filter restaurants to have only given cuisine type
+        const results = restaurants.filter(r => r.cuisine_type == cuisine);
+        callback(null, results);
+      }
+    });
+  }
+
+  /**
+   * Fetch restaurants by a neighborhood with proper error handling.
+   */
+  static fetchRestaurantByNeighborhood(neighborhood, callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Filter restaurants to have only given neighborhood
+        const results = restaurants.filter(r => r.neighborhood == neighborhood);
+        callback(null, results);
+      }
+    });
+  }
+
+  /**
+   * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
+   */
+  static fetchRestaurantByCuisineAndNeighborhoodAndFavorite(cuisine, neighborhood, favorite, callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        let results = restaurants;
+        if (cuisine != 'all') { // filter by cuisine
+          results = results.filter(r => r.cuisine_type == cuisine);
+        }
+        if (neighborhood != 'all') { // filter by neighborhood
+          results = results.filter(r => r.neighborhood == neighborhood);
+        }
+        if (favorite === true) { // filter by favorites
+          results = results.filter(r => r.is_favorite == "true");
+        }
+        callback(null, results);
+      }
+    });
+  }
+
+  /**
+   * Fetch all neighborhoods with proper error handling.
+   */
+  static fetchNeighborhoods(callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Get all neighborhoods from all restaurants
+        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+        // Remove duplicates from neighborhoods
+        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+        callback(null, uniqueNeighborhoods);
+      }
+    });
+  }
+
+  /**
+   * Fetch all cuisines with proper error handling.
+   */
+  static fetchCuisines(callback) {
+    // Fetch all restaurants
+    DBHelper.fetchRestaurants((error, restaurants) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        // Get all cuisines from all restaurants
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+        // Remove duplicates from cuisines
+        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+        callback(null, uniqueCuisines);
+      }
+    });
+  }
+
+  /**
+   * Restaurant page URL.
+   */
+  static urlForRestaurant(restaurant) {
+    return (`./restaurant.html?id=${restaurant.id}`);
+  }
+
+  /**
+   * Restaurant image URL.
+   */
+  static imageUrlForRestaurant(restaurant) {
+    if (!restaurant.hasOwnProperty('photograph')) {
+      restaurant["photograph"] = "10";
+    }
+
+    const mq_tablet = window.matchMedia("(min-width: 450px)");
+    const mq_desktop = window.matchMedia("(min-width: 800px)");
+
+    if (mq_desktop.matches) {
+      return (`/img/desktop/${restaurant.photograph}.webp`);
+    } else if (mq_tablet.matches) {
+      return (`/img/tablet/${restaurant.photograph}.webp`);
+    } else {
+      return (`/img/mobile/${restaurant.photograph}.webp`);
+    }
+  }
+
+  /**
+   * Map marker for a restaurant.
+   */
+  static mapMarkerForRestaurant(restaurant, map) {
+    const marker = new google.maps.Marker({
+      position: restaurant.latlng,
+      title: restaurant.name,
+      url: DBHelper.urlForRestaurant(restaurant),
+      map: map,
+      animation: google.maps.Animation.DROP}
+    );
+    return marker;
+  }
+  /**
+   * Add or Remove is_favorite on the server
+   */
+  static toggleFavorite(id, condition) {
+    fetch(`http://localhost:1337/restaurants/${id}/?is_favorite=${condition}`, { method: 'POST' })
+      .then(res => console.log('restaurant favorite has been updated'))
+      .then(IDBHelper.idbToggleFavorite(id, condition))
+      .then(location.reload());
+  }
+  /**
+   * Add or Remove is_favorite on the server
+   */
+  static saveOfflineReview(event, form) {
+    event.preventDefault();
+    const body = {
+      "restaurant_id": parseInt(form.id.value),
+      "name": form.dname.value,
+      "rating": parseInt(form.drating.value),
+      "comments": form.dreview.value,
+      "updatedAt": parseInt(form.ddate.value),
+      "flag": form.dflag.value,
+    };
+    IDBHelper.idbPostReview(form.id.value, body);
+    location.reload();
+  }
+}
